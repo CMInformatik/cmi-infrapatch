07f3ff11ace6b5251819d7639843e48cc1950d9a
Noah Canadea <noah.canadea@cmiag.ch>
2023-11-28 09:24:37 +0000

    feat(providers): Add Providers.

---
+++ infrapatch/core/providers/base_provider_interface.py
@@ -0,0 +1,23 @@
+from typing import Protocol, Sequence
+from rich.table import Table
+from infrapatch.core.models.versioned_resource import VersionedResource
+
+
+class BaseProviderInterface(Protocol):
+    def get_provider_name(self) -> str:
+        ...
+
+    def get_resources(self) -> Sequence[VersionedResource]:
+        ...
+
+    def patch_resource(self, resource: VersionedResource) -> VersionedResource:
+        ...
+
+    def get_rich_table(self, resources: Sequence[VersionedResource]) -> Table:
+        ...
+
+    def get_markdonw_table(self, resources: Sequence[VersionedResource]):
+        ...
+
+    def get_resources_as_dict_list(self, resources: Sequence[VersionedResource]):
+        ...
